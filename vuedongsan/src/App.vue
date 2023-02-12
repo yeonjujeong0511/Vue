2523385a28a1b057378cbbec1734cd64869d3d39
yeonju0511 <yeonjujeong0511@gmail.com>
2023-02-12 23:55:31 +0900

    반복문 및 함수

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+    <a>Products</a>
+    <a>About</a>
+  </div>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+    <button @click="increse(0)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[0] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>70 만원</p>
+    <button @click="increse(1)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[1] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>100 만원</p>
+    <button @click="increse(2)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[2] }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      신고수: [0, 0, 0],
+      메뉴들: ["Home", "Shop", "About"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+  methods: {
+    increse(i) {
+      this.신고수[i] += 1;
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.menu {
+  background: orange;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
