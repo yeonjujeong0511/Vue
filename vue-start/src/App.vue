9a64d01735d4a5706e87fca92431d81418642320
yeonju0511 <yeonjujeong0511@gmail.com>
2023-02-13 15:14:10 +0900

    todo로 v-for 학습

---
+++ vue-start/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>VUE 연습</h1>
+  <TodoList />
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-start/src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Todo List</h1>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" placeholder="할일 적기" />
+    <button>추가</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}<button @click="removeTodo(todo)">x</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+let id = 0;
+export default {
+  data() {
+    return {
+      newTodo: "",
+      todos: [
+        {
+          id: id++,
+          text: "HTML배우기",
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo });
+      this.newTodo = "";
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo);
+    },
+  },
+};
+</script>
+
+<style></style>
